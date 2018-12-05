97ba39396cdfcd1ce3d18609e5fb417c8f56333a
Hafeez Hamza <hafeez1042@gmail.com>
2018-12-05 11:56:41 +0530

    getting first element from errors array

--- src/ErrorHandlerHOC.tsx
+++ src/ErrorHandlerHOC.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { IFieldRenderProps } from "@dock365/reform";
-import Errorss from "./Errors";
+import Errors from "./Errors";
 
 const ErrorHandlerHOC = (FieldComponent: React.SFC<IFieldRenderProps & any>) => (
   (props: IFieldRenderProps) => (
     <div className={props.errors && props.errors.length > 0 ? "hasError" : ""}>
       <FieldComponent {...props} />
       {props.errors ?
-        <Errorss errors={props.errors[0]} /> : null}
+        <Errors errors={props.errors[0]} /> : null}
     </div>
   )
 );
