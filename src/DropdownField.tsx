f05ad25c4f3d965110cf80d9ea9974b3543d229a
Hafeez Hamza <hafeez1042@gmail.com>
2018-12-19 14:05:00 +0530

    Dropdown options fix

--- src/DropdownField.tsx
+++ src/DropdownField.tsx
@@ -14,6 +14,7 @@ const DropdownField: React.SFC<IFieldRenderProps & propsOverride> = (props) => (
     label={props.label}
     selectedKey={props.value}
     placeHolder={props.placeholder}
+    options={props.customProps && props.customProps.options || []}
     onChanged={(option?: IDropdownOption, index?: number) => {
       if (props.onChange)
         props.onChange(option && option.key);
