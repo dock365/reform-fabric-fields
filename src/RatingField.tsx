3195040766adcf1e659c5bb492e85afe14ebee3a
Hafeez Hamza <hafeez1042@gmail.com>
2018-12-19 15:43:20 +0530

    Rating Label

--- src/RatingField.tsx
+++ src/RatingField.tsx
@@ -13,18 +13,21 @@ type propsOverride = {
 };
 
 const RatingField: React.SFC<IFieldRenderProps & propsOverride> = (props) => (
-  <Rating
-    rating={props.value}
-    onChanged={(value) => {
-      if (props.onChange)
-        props.onChange(value);
-      if (props.onBlur)
-        props.onBlur(value);
-    }}
-    min={props.customProps && props.customProps.min || 1}
-    max={props.customProps && props.customProps.max || 5}
-    size={props.customProps && props.customProps.size || RatingSize.Large}
-  />
+  <div>
+    {props.label && <label htmlFor="" style={{ padding: "5px 0", display: "block" }}>{props.label}</label>}
+    <Rating
+      rating={props.value}
+      onChanged={(value) => {
+        if (props.onChange)
+          props.onChange(value);
+        if (props.onBlur)
+          props.onBlur(value);
+      }}
+      min={props.customProps && props.customProps.min || 1}
+      max={props.customProps && props.customProps.max || 5}
+      size={props.customProps && props.customProps.size || RatingSize.Large}
+    />
+  </div>
 
 );
 
