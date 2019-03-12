4219578cd10580d3ee1c2365637756927cc14bde
Hafeez Hamza <hafeez1042@gmail.com>
2019-03-12 10:28:45 +0530

    text field locale string

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { TextField as FabricTextField } from 'office-ui-fabric-react/lib/TextField';
+import { TextField as FabricTextField } from "office-ui-fabric-react/lib/TextField";
 import { IFieldRenderProps } from "@dock365/reform";
 import ErrorHandlerHOC from "./ErrorHandlerHOC";
 import { validationTypes } from "@dock365/validator";
@@ -9,34 +9,74 @@ type propsOverride = {
   value: string;
   className?: string;
   readOnly?: boolean;
-  onClick?: (event: React.MouseEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+  onClick?: (
+    event: React.MouseEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  customProps?: {
+    localeString?: boolean;
+  };
 };
 
-const TextField: React.SFC<IFieldRenderProps & propsOverride> = (props) => (
+const TextField: React.SFC<IFieldRenderProps & propsOverride> = props => (
   <FabricTextField
     readOnly={props.readOnly}
-    value={props.value}
+    value={
+      (props.customProps &&
+        props.customProps.localeString &&
+        props.value.toLocaleString()) ||
+      props.value
+    }
     label={props.label}
     onClick={props.onClick}
     placeholder={props.placeholder}
-    onChanged={(value) => props.onChange && props.onChange(
-      props.validationRules &&
-        props.validationRules.type === validationTypes.Number &&
-        !isNaN(Number(value)) ?
-        (value.length - value.lastIndexOf(".") === 1 ? value : Number(value))
-        : value,
-    )}
-    onBlur={(e) => {
+    onChanged={value =>
+      props.onChange &&
+      props.onChange(
+        props.validationRules &&
+          props.validationRules.type === validationTypes.Number &&
+          !isNaN(Number(value))
+          ? value.length - value.lastIndexOf(".") === 1
+            ? value
+            : Number(
+                (props.customProps &&
+                  props.customProps.localeString &&
+                  localStringToNumber(value)) ||
+                  value
+              )
+          : value
+      )
+    }
+    onBlur={e => {
       const value = e.currentTarget.value;
       if (props.onBlur)
         props.onBlur(
           props.validationRules &&
             props.validationRules.type === validationTypes.Number &&
-            !isNaN(Number(value)) ?
-            Number(value)
-            : value);
+            !isNaN(Number(value))
+            ? Number(
+                (props.customProps &&
+                  props.customProps.localeString &&
+                  localStringToNumber(value)) ||
+                  value
+              )
+            : value
+        );
     }}
   />
 );
+
+const localStringToNumber = (value: string): number => {
+  const parts = (1234.5).toLocaleString().match(/(\D+)/g);
+
+  const unformatted =
+    parts &&
+    value
+      .split(parts[0])
+      .join("")
+      .split(parts[1])
+      .join(".");
+
+  return Number(unformatted);
+};
 
 export default ErrorHandlerHOC(TextField);
