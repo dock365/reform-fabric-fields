c0beb2019bdaab8ad3175a258f0e78c9b4e68796
Hafeez Hamza <hafeez1042@gmail.com>
2019-05-14 16:49:34 +0530

    user update fix

--- src/UserPicker/UserPicker.tsx
+++ src/UserPicker/UserPicker.tsx
@@ -47,9 +47,8 @@ export class UserPicker extends React.Component<
     prevProps: IUserPickerProps,
     prevState: IUserPickerState,
   ) {
-    debugger;
     const values = this.props.values || [];
-    const prevValues = this.state.selectedUsers || prevProps.values || [];
+    const prevValues = prevProps.values || [];
     const usersNotChanged =
       prevValues.length === values.length &&
       this.state.selectedUsers.every((user: IPersonaProps & { Id?: number }) =>
@@ -106,6 +105,8 @@ export class UserPicker extends React.Component<
             selectedUsers.push(this._transformToPersona(user));
         }
         this.setState({ selectedUsers });
+      } else {
+        this.setState({ selectedUsers: [] });
       }
     } else {
       this.setState({ selectedUsers: [] });
