955f5388a9b5c24f26fb48a4084ae75a146368ef
Hafeez Hamza <hafeez1042@gmail.com>
2019-10-01 11:44:54 +0530

    localestring moved to form

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -13,30 +13,28 @@ type propsOverride = {
     event: React.MouseEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => void;
   customProps?: {
-    localeString?: boolean;
     maxLength?: number;
   };
   componentRef?: any;
 };
 
 class TextField extends React.Component<IFieldRenderProps & propsOverride, {}> {
+  static defaultProps = {
+    customProps: {},
+    value: "",
+    validationRules: {}
+  }
 
   public render() {
     return (
       <FabricTextField
         readOnly={this.props.readOnly}
-        value={
-          (this.props.customProps &&
-            this.props.customProps.localeString &&
-            (this.props.value || "").toLocaleString()) ||
-          this.props.value ||
-          ""
-        }
+        value={this.props.value}
         componentRef={this.props.componentRef}
         label={this.props.label}
         onClick={this.props.onClick}
         placeholder={this.props.placeholder}
-        maxLength={this.props.customProps && this.props.customProps.maxLength}
+        maxLength={this.props.customProps.maxLength} // For number validation also
         onChange={this._onChange}
         onBlur={this._onBlur}
       />
@@ -44,59 +42,58 @@ class TextField extends React.Component<IFieldRenderProps & propsOverride, {}> {
   }
 
   private _onChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const value: any =
-      (this.props.customProps &&
-        this.props.customProps.localeString &&
-        this._localStringToNumber(event.currentTarget.value)) ||
-        event.currentTarget.value;
-    // tslint:disable-next-line:no-unused-expression
+    // const inputValue = event.currentTarget.value;
+
+    // const value = this.props.localeString ? inputValue.toLocaleString() : inputValue;
     if (this.props.onChange) {
-      const isNumber = this.props.validationRules && this.props.validationRules.type === validationTypes.Number && !isNaN(Number(value));
-      this.props.onChange(
-        isNumber
-          ?
-          // _value.length - _value.lastIndexOf(".") === 1 ||
-          //   (_value.length - _value.lastIndexOf("0") === 1 && _value.lastIndexOf(".") > 0) ||
-            (Number(value) == value && Number(value) !== value) &&
-            !isNaN(Number(value))
-            ? value
-            : Number(value)
-          : value,
-      );
+      this.props.onChange(event.currentTarget.value);
     }
+
+    // const value: any =
+    //   (
+    //     this.props.customProps.localeString &&
+    //     this._localStringToNumber(event.currentTarget.value)) ||
+    //   event.currentTarget.value;
+    // // tslint:disable-next-line:no-unused-expression
+    // if (this.props.onChange) {
+    //   const isNumber = this.props.validationRules && this.props.validationRules.type === validationTypes.Number && !isNaN(Number(value));
+    //   this.props.onChange(
+    //     isNumber
+    //       ?
+    //       // _value.length - _value.lastIndexOf(".") === 1 ||
+    //       //   (_value.length - _value.lastIndexOf("0") === 1 && _value.lastIndexOf(".") > 0) ||
+    //       (Number(value) == value && Number(value) !== value) &&
+    //         !isNaN(Number(value))
+    //         ? value
+    //         : Number(value)
+    //       : value,
+    //   );
+    // }
   }
 
-  private _onBlur = (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const value: any =
-      (this.props.customProps &&
-        this.props.customProps.localeString &&
-        this._localStringToNumber(e.currentTarget.value)) ||
-      e.currentTarget.value;
-    if (this.props.onBlur) {
-      const isNumber = this.props.validationRules && this.props.validationRules.type === validationTypes.Number && !isNaN(Number(value));
+  private _onBlur = (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    // const inputValue = event.currentTarget.value;
 
-      // this.props.onBlur(value)
-        this.props.onBlur(
-          isNumber
-            ? (Number(value) == value) ? value :Number(value)
-            : value,
-        );
+    // const value = this.props.localeString ? inputValue.toLocaleString() : inputValue;
+    if (this.props.onChange) {
+      this.props.onChange(event.currentTarget.value);
     }
+    // const value: any =
+    //   (
+    //     this.props.customProps.localeString &&
+    //     this._localStringToNumber(e.currentTarget.value)) ||
+    //   e.currentTarget.value;
+    // if (this.props.onBlur) {
+    //   const isNumber = this.props.validationRules && this.props.validationRules.type === validationTypes.Number && !isNaN(Number(value));
+
+    //   // this.props.onBlur(value)
+    //   this.props.onBlur(
+    //     isNumber
+    //       ? (Number(value) == value) ? value : Number(value)
+    //       : value,
+    //   );
+    // }
   }
-  private _localStringToNumber = (value: string): string | null => {
-    const parts = (1234.5).toLocaleString()
-      .match(/(\D+)/g);
-
-    const unformatted =
-      parts &&
-      value
-        .split(parts[0])
-        .join("")
-        .split(parts[1])
-        .join(".");
-
-    return unformatted;
-  };
 }
 
 export default ErrorHandlerHOC(TextField);
