1e43df9db096446c4375ab57b5827359791af0ff
Hafeez Hamza <hafeez.hamza@global-infonet.com>
2020-03-31 17:52:02 +0530

    idea added to gitignore and option picker using react ref

--- src/OptionsPicker/OptionsPicker.tsx
+++ src/OptionsPicker/OptionsPicker.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { IOptionsPickerProps } from './IOptionsPickerProps';
 import { Callout } from 'office-ui-fabric-react/lib/Callout';
-import { createRef } from '@uifabric/utilities/lib';
 import { IOptionsPickerState } from './IOptionsPickerState';
 import OptionsPickerMenu from './OptionsPickerMenu/OptionsPickerMenu';
 import { IOptionsPickerOption } from './OptionsPickerMenu/IOptionsPickerMenuProps';
 
 export default class OptionsPicker extends React.Component<IOptionsPickerProps, IOptionsPickerState> {
-  private _menuButtonElement = createRef<HTMLElement>();
+  private _menuButtonElement = React.createRef<HTMLElement>();
 
   constructor(props: IOptionsPickerProps) {
     super(props);
